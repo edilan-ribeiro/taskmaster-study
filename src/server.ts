6d6483835cd74b1fcaab806f9ae6046cb87e9f84
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-05-23 20:02:36 -0300

    feat: add documentation to user and general tasks
    This will make possible to test user and gereral tasks routes with open api, not all routes are semantic correctly but will do for now

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,18 @@ import dbConnection from './database/connection'
 import taskListRoute from './routes/tasks/taskListRoute'
 import simpleNoteRoute from './routes/tasks/taskTypes/simpleNoteRoute'
 import usersRoute from './routes/users/users'
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "./swagger.json";
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/user', usersRoute)
 app.use('/tasks', taskListRoute)
 app.use('/task/simple-note', simpleNoteRoute)
-
 
 
 dbConnection().then(() => {
