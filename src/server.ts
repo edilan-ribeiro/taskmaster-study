c8bcd1af99b3ef0c924a8ecaaf8c8b3e9d010ddf
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-05-23 15:19:39 -0300

    feat: add the previously created routes to the server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express'
 import dbConnection from './database/connection'
-import taskListRoute  from './routes/tasks/taskListRoute'
+import taskListRoute from './routes/tasks/taskListRoute'
+import simpleNoteRoute from './routes/tasks/taskTypes/simpleNoteRoute'
+import usersRoute from './routes/users/users'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
+app.use('/user', usersRoute)
 app.use('/tasks', taskListRoute)
+app.use('/task/simple-note', simpleNoteRoute)
+
 
 
 dbConnection().then(() => {
