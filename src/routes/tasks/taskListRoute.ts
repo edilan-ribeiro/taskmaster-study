b741c0015f63e6f779fdd614ae249a4b5a1a4ff9
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-05-23 15:18:00 -0300

    feat: add initial task route
    This route will provide access to tasks that the user previously saved in the database and will also allow the user to archive or delete the task

---
+++ src/routes/tasks/taskListRoute.ts
@@ -0,0 +1,97 @@
+import express from 'express'
+import { Task } from '../../database/models/tasks/taskModel'
+import mongoose, { SortOrder } from 'mongoose'
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+	const { order, limit, offset } = req.query
+	const { userId } = req.body
+
+	const sortOrder: SortOrder = order === 'desc'? -1 : 1;
+
+	try {
+		
+		if (userId) {
+			const tasks = await Task.find({ userId }).sort({ createdAt: sortOrder }).limit(Number(limit)).skip(Number(offset))
+
+			res.status(200).json(tasks)
+		} else {
+			res.status(401).json({ message: 'User not found' })
+		}
+	} catch (err) {
+		console.error(`Error retrieving data : ${err}`)
+		res.status(500).json({ message: 'Server failed to fetch the data' })
+	}
+})
+
+router.get('/:id', async (req, res) => {
+	const taskId = req.params.id
+
+	if (!mongoose.Types.ObjectId.isValid(taskId)) {
+		return res.status(400).json({ message: 'Invalid task ID' })
+	}
+
+	try {
+		const getTask = await Task.findById(taskId)
+
+		if (getTask) {
+			res.status(200).json(getTask)
+		} else {
+			res.status(404).json({ message: 'Task not found' })
+		}
+	} catch (err) {
+		console.error(`Error getting id data : ${err}`)
+		res.status(500).json({ message: 'Failed to get the task' })
+	}
+})
+
+router.put('/:id', async (req, res) => {
+	const taskId = req.params.id
+
+	const { isArchived } = req.body
+
+	if (!mongoose.Types.ObjectId.isValid(taskId)) {
+		return res.status(400).json({ message: 'Invalid task ID' })
+	}
+
+	try {
+		const archiveTask = await Task.findByIdAndUpdate(
+			taskId,
+			{ isArchived: isArchived },
+			{ new: true, runValidators: true }
+		)
+
+		if (archiveTask) {
+			res.status(200).json(archiveTask)
+		} else {
+			res.status(404).json({ message: 'Task not found' })
+		}
+	} catch (err) {
+		console.error(`Error editing data : ${err}`)
+		res.status(500).json({ message: 'Failed to archive record' })
+	}
+})
+
+router.delete('/:id', async (req, res) => {
+	const taskId = req.params.id
+
+	if (!mongoose.Types.ObjectId.isValid(taskId)) {
+		return res.status(400).json({ message: 'Invalid task ID' })
+	}
+
+	try {
+		const deleteTask = await Task.findByIdAndDelete(taskId)
+
+		if (deleteTask) {
+			res.status(200).json({ message: 'Task removed from database!' })
+		} else {
+			res.status(404).json({ message: 'Task not found' })
+		}
+	} catch (err) {
+		console.error(`Error deleting data : ${err}`)
+		res.status(500).json({ message: 'Failed to delete record' })
+	}
+})
+
+export default router
